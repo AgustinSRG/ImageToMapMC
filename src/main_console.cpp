32b13c4600f2256747e38d75962426dae4e5666e
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2021-08-21 15:55:22 +0200

    Setup project for 2 targets
    - Win app
    - Console app

---
+++ src/main_console.cpp
@@ -0,0 +1,44 @@
+/*
+ * This file is part of ImageToMapMC project
+ * 
+ * Copyright (c) 2021 Agustin San Roman
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "main_console.h"
+#include <iostream>
+#include <string>
+#include "mapart/map_art.h"
+
+using namespace std;
+using namespace colors;
+using namespace minecraft;
+using namespace mapart;
+
+/**
+ * @brief  Main (Console)
+ * @note   
+ * @param  argc: 
+ * @param  argv: 
+ * @retval 
+ */
+int main(int argc, char ** argv) {
+    cout << "This is the console version of mcmap!" << endl;
+    return 0;
+}
