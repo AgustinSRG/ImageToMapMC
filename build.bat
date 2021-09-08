e1c33440599d8f5b6c6ac27c78058562702c2d4d
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2021-09-08 17:34:28 +0200

    Windows: Builds for 32 and 64 bit archs

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo on
 
-call cmake -DCMAKE_BUILD_TYPE=Release -S. -Brelease
+call cmake -DCMAKE_BUILD_TYPE=Release -A x64 -S. -Brelease
+call cmake -DCMAKE_BUILD_TYPE=Release -A Win32 -S. -Brelease32
 
 call cmake --build release --config Release
+call cmake --build release32 --config Release
