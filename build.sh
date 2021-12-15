e5cd76b764c159428e31c8a1335decd6636c1cb1
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2021-12-15 19:53:10 +0100

    Fix minor issue with bash build script

--- build.sh
+++ build.sh
@@ -2,21 +2,19 @@
 
 # Build script for linux
 
-VERSION=1.2.1
-
 cmake -DCMAKE_BUILD_TYPE=Release -Ssrc -Brelease
 
 cmake --build release --config Release
 
 rm -rf release/bin || true
 
-rm -rf release/ImageToMapMC-$1.2.1-linux-x64.tar.gz || true
+rm -rf release/ImageToMapMC-1.2.1-linux-x64.tar.gz || true
 
 mkdir release/bin
 
 cp release/mcmap release/bin/mcmap
 cp release/mcmap-gui release/bin/mcmap-gui
 
-tar -cvzf release/ImageToMapMC-$1.2.1-linux-x64.tar.gz release/bin
+tar -cvzf release/ImageToMapMC-1.2.1-linux-x64.tar.gz release/bin
 
-echo "DONE: Created release ImageToMapMC-$1.2.1-linux-x64.tar.gz"
+echo "DONE: Created release ImageToMapMC-1.2.1-linux-x64.tar.gz"
--- script-templates/build.sh
+++ script-templates/build.sh
@@ -2,21 +2,19 @@
 
 # Build script for linux
 
-VERSION={VERSION}
-
 cmake -DCMAKE_BUILD_TYPE=Release -Ssrc -Brelease
 
 cmake --build release --config Release
 
 rm -rf release/bin || true
 
-rm -rf release/ImageToMapMC-${VERSION}-linux-x64.tar.gz || true
+rm -rf release/ImageToMapMC-{VERSION}-linux-x64.tar.gz || true
 
 mkdir release/bin
 
 cp release/mcmap release/bin/mcmap
 cp release/mcmap-gui release/bin/mcmap-gui
 
-tar -cvzf release/ImageToMapMC-${VERSION}-linux-x64.tar.gz release/bin
+tar -cvzf release/ImageToMapMC-{VERSION}-linux-x64.tar.gz release/bin
 
-echo "DONE: Created release ImageToMapMC-${VERSION}-linux-x64.tar.gz"
+echo "DONE: Created release ImageToMapMC-{VERSION}-linux-x64.tar.gz"
