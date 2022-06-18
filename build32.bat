3dab3ba8813747d13f90e0243fd6edda3c0b0113
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-06-18 11:43:25 +0200

    Bump version

--- build32.bat
+++ build32.bat
@@ -4,4 +4,4 @@ call cmake -DCMAKE_BUILD_TYPE=Release -A Win32 -Ssrc -Brelease32
 
 call cmake --build release32 --config Release
 
-call zip -r release32/ImageToMapMC-1.2.1-Windows-Win32.zip release32/Release
+call zip -r release32/ImageToMapMC-1.2.2-Windows-Win32.zip release32/Release
