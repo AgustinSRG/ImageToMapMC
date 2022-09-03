8d64177dea09d6071b193b748a503b5b6f682533
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-09-03 20:46:44 +0200

    Bump version to 1.3.0

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ call cmake -DCMAKE_BUILD_TYPE=Release -A x64 -Ssrc -Brelease
 
 call cmake --build release --config Release
 
-call zip -r release/ImageToMapMC-1.2.2-Windows-x64.zip release/Release
+call zip -r release/ImageToMapMC-1.3.0-Windows-x64.zip release/Release
--- make-wix-installer.bat
+++ make-wix-installer.bat
@@ -2,4 +2,4 @@
 
 call candle -ext WixUIExtension -ext WixUtilExtension -arch x64 Product.wxs -o release\Product.wixobj
 
-call light release\Product.wixobj -o release\ImageToMapMC-1.2.2-x64.msi -ext WixUIExtension -ext WixUtilExtension
+call light release\Product.wixobj -o release\ImageToMapMC-1.3.0-x64.msi -ext WixUIExtension -ext WixUtilExtension
