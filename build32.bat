f91217dedc7ca619e2697e74a217ec21b434000f
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-01-02 18:38:03 +0100

    Bump version to 1.3.1

--- build32.bat
+++ build32.bat
@@ -4,4 +4,4 @@ call cmake -DCMAKE_BUILD_TYPE=Release -A Win32 -Ssrc -Brelease32
 
 call cmake --build release32 --config Release
 
-call zip -r release32/ImageToMapMC-1.3.0-Windows-Win32.zip release32/Release
+call zip -r release32/ImageToMapMC-1.3.1-Windows-Win32.zip release32/Release
