0e6073f60656ff6966c2ddc2a8b94485f231a14a
AgustinSRG <agustinsanromanguzman@gmail.com>
2025-06-19 18:18:41 +0200

    Bump version

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ call cmake -DCMAKE_BUILD_TYPE=Release -A x64 -Ssrc -Brelease
 
 call cmake --build release --config Release
 
-call zip -r release/ImageToMapMC-1.3.7-Windows-x64.zip release/Release
+call zip -r release/ImageToMapMC-1.4.0-Windows-x64.zip release/Release
--- build32.bat
+++ build32.bat
@@ -4,4 +4,4 @@ call cmake -DCMAKE_BUILD_TYPE=Release -A Win32 -Ssrc -Brelease32
 
 call cmake --build release32 --config Release
 
-call zip -r release32/ImageToMapMC-1.3.7-Windows-Win32.zip release32/Release
+call zip -r release32/ImageToMapMC-1.4.0-Windows-Win32.zip release32/Release
