71366bf1b10fc95585ba097ad926bb0aaf7c2cb9
AgustinSRG <agustinsanromanguzman@gmail.com>
2025-07-26 22:42:02 +0200

    Bump version (1.6.3)

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ call cmake -DCMAKE_BUILD_TYPE=Release -DWIN_RELEASE=1 -A x64 -Ssrc -Brelease
 
 call cmake --build release --config Release
 
-call zip -r release/ImageToMapMC-1.6.2-Windows-x64.zip release/Release
+call zip -r release/ImageToMapMC-1.6.3-Windows-x64.zip release/Release
